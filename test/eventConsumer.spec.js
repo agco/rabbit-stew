e27dcdf4b843e9ec9cfae135849576d950770232
Stephen Parker <shurakaisoft@gmail.com>
2015-11-19 09:14:54 -0500

    updated API, tests not yet working

--- test/eventConsumer.spec.js
+++ test/eventConsumer.spec.js
@@ -29,7 +29,7 @@ describe('RabbitStew (RabbitMQ generic data consumer) Module', function () {
 		};
 	});
 
-	describe('The createConsumers API', function () {
+	describe('The createConsumers function', function () {
 		it('should throw an error if the first argument is not a jackrabbit exchange object', function (done) {
 			try {
 				RabbitStew.createConsumers();
@@ -85,7 +85,13 @@ describe('RabbitStew (RabbitMQ generic data consumer) Module', function () {
 			}
 		});
 
-		it('should return a queue object');
+		it('should return a queue object', function () {
+			var queue = RabbitStew.createConsumers(exchange, options,consumers);
+
+			queue.should.have.property('consume').and.be.a.Function;
+			queue.should.have.property('options').and.be.an.Object;
+			queue.options.should.have.property('consumers').and.be.an.Object;
+		});
 
 		// TODO: it would be nice to destroy queues once created for cleanup but jackrabbit doesn't provide this.
 		it.skip('should return a destroyable queue object', function () {
@@ -113,7 +119,7 @@ describe('RabbitStew (RabbitMQ generic data consumer) Module', function () {
 		it('should not retry payloads with invalid routing keys')
 	});
 
-	describe('the exchange API', function () {
+	describe('the exchange function', function () {
 		it('should be a function', function () {
 			RabbitStew.exchange.should.be.a.Function;
 		});
@@ -126,6 +132,92 @@ describe('RabbitStew (RabbitMQ generic data consumer) Module', function () {
 			exchange.should.have.property('connect').and.be.a.Function;
 			exchange.should.have.property('publish').and.be.a.Function;
 		});
+	});
+
+	describe('the queue function', function () {
+		var queue;
+		// for returning a jackrabbit queue
+		it('should return a jackrabbit queue object', function () {
+			queue = RabbitStew.queue(exchange, { name: 'jackrabbitQueue', keys: [ 'pass.through', 'routing.keys']});
+			queue.should.have.property('consume').and.be.a.Function;
+			queue.should.have.property('options').and.be.an.Object;
+			queue.options.should.have.property('keys').and.be.an.Array
+			queue.options.keys[0].should.equal('pass.through');
+		});
+
+		describe('the consume function', function () {
+			var key;
+			var options;
+			beforeEach(function () {
+				key = 'consume.function.test';
+				options = {
+					name: 'test.consume.function',
+					durable: false,
+					exclusive: false,
+					key: [ key ]
+				}
+				queue = RabbitStew.queue(exchange, options);
+			});
+
+			it('should create a single consumer handler to the queue.', function (done) {
+				var payload = 'test data for single consumer handler';
+
+				queue.consume(function handler(data) {
+					data.should.equal(payload);
+					done();
+					return Promise.resolve('ack');
+				});
+				exchange.publish(payload, { key: key });
+			});
+			it('should call the same handler for all routingKeys defined for the queue', function (done) {
+				var payloadA = 'payload for consumer.A';
+				var payloadB = 'payload for consumer.B';
+				var routingKeyA = 'consumer.A';
+				var routingKeyB = 'consumer.B';
+				var receivedA = false;
+				var receivedB = false;
+
+				delete options.key;
+				options.keys = [ routingKeyA, routingKeyB ];
+				queue = RabbitStew.queue(exchange, options);
+				queue.consume(function handler(data) {
+					if (data === payloadA) {
+						if (receivedA) {
+							done('ERROR: payload for routingKeyA received multiple times');
+							return Promise.reject('nack');
+						} else {
+							payloadA = true;
+						}
+					}
+					if (data === payloadB) {
+						if (receivedB) {
+							done('ERROR: payload for routingKeyB received multiple times');
+							return Promise.reject('nack');
+						} else {
+							payloadB = true;
+						}
+					}
+					if (receivedA && receivedB) {
+						done();
+					}
+					return Promise.resolve('ack')
+				});
+				exchange.publish(payloadA, { key: routingKeyA });
+				exchange.publish(payloadB, { key: routingKeyB });
+			});
+			it('should not call the handler for routingKeys not defined for the queue', function (done) {
+				var payload = 'this should never be seen';
+
+				queue.consume(function handler(data) {
+					done('ERROR: this handler should not be called as the routingKey is different.');
+					return Promise.reject('nack');
+				});
+
+				exchange.publish(payload, { key: 'invalid.consume.function.test' });
+				setTimeout(done, 500);
+			});
+		});
+
 	});
 
 });
